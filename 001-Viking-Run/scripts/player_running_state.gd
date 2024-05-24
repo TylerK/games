80e570a1533008a92118f9a8b377404c434c3549
Tyler Kelley <tylerckelley@gmail.com>
2024-05-24 16:32:45 -0400

    Add sliding

--- 001-Viking-Run/scripts/player_running_state.gd
+++ 001-Viking-Run/scripts/player_running_state.gd
@@ -8,10 +8,10 @@ func enter():
 	$"../../PlayerSprite".play("run")
 
 func physics_update(_delta):
-	if Input.is_action_just_pressed("attack"):
-		print('attack???????')
-		transitioned.emit("Attacking")
 	if Input.is_action_just_pressed("jump"):
 		transitioned.emit('Jumping')
-
+	if Input.is_action_just_pressed("attack"):
+		transitioned.emit('Attacking')
+	if Input.is_action_just_pressed("slide"):
+		transitioned.emit('Sliding')
 
--- 001-Viking-Run/scripts/player_sliding_state.gd
+++ 001-Viking-Run/scripts/player_sliding_state.gd
@@ -2,6 +2,16 @@ extends State
 
 class_name SlidingState
 
-func ready():
-	print('Sliding...')
-	pass
+@onready var player_sprite: AnimatedSprite2D = $"../../PlayerSprite";
+@onready var timer = $"../../SlideTimer"
+
+func enter():
+	timer.start()
+	player_sprite.play("slide")
+
+func physics_update(_delta):
+	if player_sprite.frame == 3:
+		player_sprite.pause()
+
+func _on_slide_timer_timeout():
+	transitioned.emit("Running")
