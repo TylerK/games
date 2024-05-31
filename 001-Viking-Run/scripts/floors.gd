d2ff0f7fe4172924c1f6400e2c525a0abb477914
Tyler Kelley <tylerckelley@gmail.com>
2024-05-31 14:08:14 -0400

    Cleanup

--- 001-Viking-Run/scripts/floors.gd
+++ 001-Viking-Run/scripts/floors.gd
@@ -1,34 +1,48 @@
 extends Node2D
 
 var platforms = []
+const SCREEN_WIDTH = 640
+const PLATFORM_SPACING = 640
 
 func _ready() -> void:
-	reset_platforms()
+	initialize_platforms()
 
-func _process(delta: float) -> void:
-	for platform in platforms:
-		platform.position.x -= Globals.scroll_speed * delta
-		#print(platform.name, ' ', platform.position.x)
-		var platform_width = platform.get_used_rect().size.x * platform.rendering_quadrant_size
-		if platform.position.x + platform_width < 0.0:
-			platforms.pop_front()
+func _physics_process(delta: float) -> void:
+	move_platforms(delta)
+	recycle_platforms_if_needed()
 
-	if platforms.size() <= 1:
-		reset_platforms()
-
-
-func reset_platforms():
-	print('resetting ', platforms)
-	var count = 0
+func initialize_platforms() -> void:
+	platforms.clear()
 	var tempArray = []
 	tempArray.append_array(get_children())
 	tempArray.shuffle()
 
-	for platform in tempArray:
-		platform.position = Vector2(640 * count, 360)
-		count += 1
+	for i in range(tempArray.size()):
+		var platform = tempArray[i]
+		platform.position = Vector2(SCREEN_WIDTH * i, 360)
+		platforms.append(platform)
 
-	platforms.append_array(tempArray)
+func move_platforms(delta: float) -> void:
+	for platform in platforms:
+		platform.position.x -= Globals.scroll_speed * delta
+
+func recycle_platforms_if_needed() -> void:
+	var last_platform = platforms[-1]
+	var new_platforms = []
 
 	for platform in platforms:
-		print(platform.name, ':: ', platform.position)
+		if platform.position.x + platform.get_used_rect().size.x < -608:
+			new_platforms.append(platform)
+			platforms.erase(platform)
+
+	if platforms.size() <= 2:
+		randomize_and_append_platforms(last_platform, new_platforms)
+
+func randomize_and_append_platforms(last_platform, new_platforms) -> void:
+	new_platforms.shuffle()
+	var start_x = last_platform.position.x + PLATFORM_SPACING
+
+	for platform in new_platforms:
+		platform.position = Vector2(start_x, 360)
+		start_x += PLATFORM_SPACING
+		platforms.append(platform)
